9d253a1e921f133a29dbd40f2a3d9882c68443e0
acconnitt <41142987+acconnitt@users.noreply.github.com>
2021-05-27 12:17:48 -0400

    Update console.py

--- mastermind/game/console.py
+++ mastermind/game/console.py
@@ -1,1 +1,14 @@
-# The console class outputs and prints various things that the director class tells it to do.
+class Console:
+    
+    def print_blank_line(self):
+        #Print blank line
+        print("--------------------")
+
+    def print_player(self, player): 
+        #Print player's name and the start guess and hint string
+        print(f"Player {player.get_name()}: {player.get_guess()}, {player.get_hint()}")
+
+    def print_turn(self,player):
+        print(f"{player.get_name}'s turn: ")
+
+    
