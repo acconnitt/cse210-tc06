3088f76af4312a5b0c36a31b12cd1cd890d4f473
acconnitt <41142987+acconnitt@users.noreply.github.com>
2021-05-27 12:23:31 -0400

    Update console.py

--- mastermind/game/console.py
+++ mastermind/game/console.py
@@ -1,14 +1,15 @@
 class Console:
-    
+
     def print_blank_line(self):
-        #Print blank line
+        # Print blank line
         print("--------------------")
 
-    def print_player(self, player): 
-        #Print player's name and the start guess and hint string
-        print(f"Player {player.get_name()}: {player.get_guess()}, {player.get_hint()}")
+    def print_player(self, player):
+        # Print player's name and the start guess and hint string
+        print(
+            f"Player {player.get_name()}: {player.get_guess()}, {player.get_hint()}")
 
-    def print_turn(self,player):
+    def print_turn(self, player):
+        # Prints player's turn
         print(f"{player.get_name}'s turn: ")
 
-    
