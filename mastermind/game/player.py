b29885d92ef8a82a0e29efb7c5f2730659149fe8
benjaminjalebeau <72671113+benjaminjalebeau@users.noreply.github.com>
2021-05-28 20:41:56 -0600

    Update player.py

--- mastermind/game/player.py
+++ mastermind/game/player.py
@@ -3,34 +3,20 @@ class Player:
     """
     """
 
-    def __init__(self, name):
+    def __init__(self, name, guess, code):
         """
         """
         self._name = name
-        self._guess = "----"
-        self._hint = "****"
-
-    def get_guess(self):
-        """
-        """
-        return self._guess
+        self.guess = guess
+        self.code = code
 
     def get_name(self):
         """
         """
         return self._name
 
-    def set_guess(self, guess):
-        """
-        """
-        self._guess = guess
-
-    def set_hint(self, hint):
-        """
-        """
-        self._hint = hint
+    def get_guess(self):
+        return self.guess.get_guess()
 
     def get_hint(self):
-        """
-        """
-        return self._hint
+        return self.guess.hint
