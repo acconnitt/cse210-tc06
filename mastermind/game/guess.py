11e9db76cb41ac17fc3aeac8fde117696d334a55
benjaminjalebeau <72671113+benjaminjalebeau@users.noreply.github.com>
2021-05-28 20:40:13 -0600

    Update guess.py

--- mastermind/game/guess.py
+++ mastermind/game/guess.py
@@ -1,28 +1,38 @@
 # The guess class gets and stores the last guess of each player.
 class Guess:
 
-    def __init__(self, code, guess="____"):
+    def __init__(self ,code, guess="____"):
         """The class constructor.
         """
-        self.__code = str(code)
         self.__guess = str(guess)
         self.hint = "****"
+        self.code = str(code)
 
     def check_guess(self):
         index = 0
         x = list(self.hint)
-        for letter in self.__code:
+        for letter in str(self.code):
             if letter == self.__guess[index]:
                 x[index] = "X"
-            elif self.__guess[index] in self.__code:
+            elif self.__guess[index] in self.code:
                 x[index] = "O"
             index += 1
         self.hint = "".join(x)
 
-    def get_code(self):
-
-        return self.__code
+    def set_guess(self, guess):
+        self.__guess = guess
 
     def get_guess(self):
 
         return self.__guess
+
+    def check_win(self):
+        win = False
+        for letter in self.hint:
+            if letter == "X":
+                win = True
+            else:
+                win = False
+                break
+        return win
+
