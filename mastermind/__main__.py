04e1265336632cdfa68f93bb04fc8f2339d7800b
acconnitt <41142987+acconnitt@users.noreply.github.com>
2021-05-30 00:55:46 -0400

    Update __main__.py

--- mastermind/__main__.py
+++ mastermind/__main__.py
@@ -1,4 +1,4 @@
 from game.director import Director
 
-director = Director()
-director.start_game()
+director = Director() # Calls the Director Class
+director.start_game() # Calls start_game method from Director Class
