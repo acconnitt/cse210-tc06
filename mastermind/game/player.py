8824efcd4701b66735696c52a68d3dd6b18033d0
acconnitt <41142987+acconnitt@users.noreply.github.com>
2021-05-31 13:41:45 -0400

    Update player.py

--- mastermind/game/player.py
+++ mastermind/game/player.py
@@ -1,36 +1,24 @@
-# The player class gets the names of each player and stores them.
+
 class Player:
-    """
+    """ The player class gets the names of each player and stores them.
     """
 
     def __init__(self, name):
-        """
-        """
         self._name = name
         self._guess = "----"
         self._hint = "****"
 
     def get_guess(self):
-        """
-        """
-        return self._guess
+        return self._guess # Returns guess
 
     def get_name(self):
-        """
-        """
-        return self._name
+        return self._name # Returns name
 
     def set_guess(self, guess):
-        """
-        """
-        self._guess = guess
+        self._guess = guess # Set new guess
 
     def set_hint(self, hint):
-        """
-        """
-        self._hint = hint
+        self._hint = hint # Set new hint
 
     def get_hint(self):
-        """
-        """
-        return self._hint
+        return self._hint # Returns hint
