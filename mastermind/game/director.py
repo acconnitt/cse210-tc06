48db3daab1e1a3d7b85fe063e41323bad5ee688b
acconnitt <41142987+acconnitt@users.noreply.github.com>
2021-05-31 11:45:02 -0400

    Update director.py

--- mastermind/game/director.py
+++ mastermind/game/director.py
@@ -1,23 +1,21 @@
-from game.board import Board
-from game.console import Console
-from game.move import Move
+# The director handles the playing of the game and calls the other classes together.
+from game.code import Code
 from game.player import Player
 from game.roster import Roster
+from game.guess import Guess
+from game.console import Console
 
 
 class Director:
-    """A code template for a person who directs the game. The responsibility of 
-    this class of objects is to control the sequence of play.
+    """The responsibility of this class of objects is to control the sequence of play.
 
     Stereotype:
         Controller
 
     Attributes:
-        board (Hunter): An instance of the class of objects known as Board.
-        console (Console): An instance of the class of objects known as Console.
-        keep_playing (boolean): Whether or not the game can continue.
-        move (Rabbit): An instance of the class of objects known as Move.
         roster (Roster): An instance of the class of objects known as Roster.
+        console (Console): An instance of the class of objects known as Console.
+        keep_playing (boolean): Whether or not the game can continue. 
     """
 
     def __init__(self):
@@ -26,11 +24,10 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
-        self._board = Board()
-        self._console = Console()
-        self._keep_playing = True
-        self._move = None
-        self._roster = Roster()
+        self.roster = Roster() # Calls the Roster class
+        self.console = Console() # Calls the Console Class
+        self.keep_playing = True # Keep playing while is true
+        self.win = False # Starts as false while player hasn't won
 
     def start_game(self):
         """Starts the game loop to control the sequence of play.
@@ -38,62 +35,70 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
-        self._prepare_game()
-        while self._keep_playing:
+        self.prepare_game() # Calls the prepare_game method
+        self.roster.next_player() # Calls next_player method from Roster class
+        while self.keep_playing:
+            print()
             self._get_inputs()
             self._do_updates()
-            self._do_outputs()
+            self.roster.next_player()
 
-    def _prepare_game(self):
+    def _get_inputs(self):
+        """Gets the inputs at the beginning of each round of play.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        player = self.roster.get_current()
+        self.console.print_blank_line()
+        self.console.print_players(self.roster)
+        self.console.print_blank_line()
+        self.console.print_turn(player)
+
+        valid_guess = False
+        """Ask for player guess 
+        Also checks if player imput is valid
+        """
+        while not valid_guess:
+            guess = input("What is your guess? ")
+            for i in guess:
+                if i.isalpha():
+                    print(
+                        "Your guess was invalid because it contained letters, please try again. ")
+                    break
+            if len(guess) != 4:
+                print(
+                    "Your guess was invalid because it wasn't 4 characters long, please try again.")
+            else:
+                valid_guess = True
+
+        player.guess.set_guess(guess)
+        player.guess.check_guess()
+        self.win = player.guess.check_win()
+
+    def _do_updates(self):
+        """Check if game is player won the game
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if self.win:
+            self.console.print_win(self.roster.get_current())
+            self.keep_playing = False
+        elif not self.win:
+            pass
+
+    def prepare_game(self):
         """Prepares the game before it begins. In this case, that means getting the player names and adding them to the roster.
 
         Args:
             self (Director): An instance of Director.
         """
-        for n in range(2):
-            name = self._console.read(f"Enter a name for player {n + 1}: ")
-            player = Player(name)
-            self._roster.add_player(player)
-
-    def _get_inputs(self):
-        """Gets the inputs at the beginning of each round of play. In this case,
-        that means getting the move from the current player.
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        # display the game board
-        board = self._board.to_string()
-        self._console.write(board)
-        # get next player's move
-        player = self._roster.get_current()
-        self._console.write(f"{player.get_name()}'s turn:")
-        pile = self._console.read_number("What pile to remove from? ")
-        stones = self._console.read_number("How many stones to remove? ")
-        move = Move(stones, pile)
-        player.set_move(move)
-
-    def _do_updates(self):
-        """Updates the important game information for each round of play. In 
-        this case, that means updating the board with the current move.
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        player = self._roster.get_current()
-        move = player.get_move()
-        self._board.apply(move)
-
-    def _do_outputs(self):
-        """Outputs the important game information for each round of play. In 
-        this case, that means checking if there are stones left and declaring the winner.
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        if self._board.is_empty():
-            winner = self._roster.get_current()
-            name = winner.get_name()
-            print(f"\n{name} won!")
-            self._keep_playing = False
-        self._roster.next_player()
+        code = Code() #Calls the Code class
+        the_code = code.get_code() #Calls the get_code method from Code class
+        # Loop twice to get two players name, guess and code
+        for n in range(2): 
+            name = input(f"Enter a name for player {n + 1}: ")
+            guess = Guess(the_code)
+            player = Player(name, guess, code)
+            self.roster.add_player(player)
